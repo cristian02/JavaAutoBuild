bc0eba8c863dc2bf76f1391409e1c180a4954bdd
cristian02 <crismh2@hotmail.com>
2019-09-30 16:00:57 -0600

    esto es una prueba

--- src/javaautobuild/JavaAutoBuild.java
+++ src/javaautobuild/JavaAutoBuild.java
@@ -17,7 +17,7 @@ public class JavaAutoBuild {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("This is a test");
-        System.out.println("Hello");
+        System.out.println("Hello"); 
     }
     
 }

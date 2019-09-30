486217220ce62e5c79f6545b46173499d6c18de8
cristian02 <crismh2@hotmail.com>
2019-09-30 16:03:40 -0600

    esto es una prueba

--- src/javaautobuild/JavaAutoBuild.java
+++ src/javaautobuild/JavaAutoBuild.java
@@ -18,6 +18,7 @@ public class JavaAutoBuild {
         // TODO code application logic here
         System.out.println("This is a test");
         System.out.println("Hello"); 
+        System.out.println("Docker test");
     }
     
 }
